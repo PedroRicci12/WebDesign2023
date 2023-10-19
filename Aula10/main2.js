3412d652a526a4bce2321884c42a68f7b074df5d
Fabio Rodrigo Colombini <fabiorcolombini@gmail.com>
2023-10-19 09:16:21 -0300

    Aula 10 teoria

---
+++ Aula10/main2.js
@@ -0,0 +1,33 @@
+let buttons = document.querySelectorAll('button');
+
+/*for (let i = 0; i < buttons.length; i++) {
+    console.log(buttons[i].value);
+}
+
+for (let button of buttons) {
+    console.log(button.value);
+}
+
+buttons.forEach(function (button) {
+    console.log(`Cliquei o botao ${button.value} mais uma vez`);
+});
+
+buttons.forEach(button => {
+    console.log(button.value);
+});*/
+
+/*let btns = document.querySelectorAll('a','.btn');
+let currentUrl = window.location.href;
+
+for (let btn of btns) {
+    if (btn.href === currentUrl) {
+        btn.classList.add('active');
+        break;
+    }
+}*/
+
+
+
+
+
+
